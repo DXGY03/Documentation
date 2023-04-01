7c44135461b031b7919925c7b60256da2a402248
Diego <diegooliveromatos@gmail.com>
2023-03-31 22:42:48 -0400

    Elaborando base de datos y servidor con pagina index

---
+++ app.py
@@ -0,0 +1,21 @@
+import sqlite3
+from flask import Flask, render_template, request,flash,redirect,url_for
+
+
+#comienzan las rutas
+#holaaaaaaaaaaaaa
+
+app= Flask (__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+   
+  
